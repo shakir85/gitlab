822f4621f2f14c9ec2489d5b78ccdc8c0a1e02a1
shakir <sralmasaari@gmail.com>
2023-06-15 14:32:31 -0700

    Initial classes

---
+++ runners/Actions.py
@@ -0,0 +1,8 @@
+"""
+Wrappers for Gitlab API requests - Perform actions on CI Runners
+"""
+from Data import RunnerData
+
+class RunnerActions(RunnerData):
+    "Actions = enable, disable, delete"
+    pass
---
+++ runners/Data.py
@@ -0,0 +1,43 @@
+"""
+Wrappers for Gitlab API requests - Retrieve CI Runners data
+"""
+import requests
+
+
+class RunnerData:
+    def __init__(self, token):
+        self.token: str = token
+        self.headers: dict = {"PRIVATE-TOKEN": self.token}
+        self.__base_url = "https://gitlab.com/api/v4/runners"
+
+    def get_runners_data(self, all: bool = False, paused: bool = False, online_only: bool = False) -> list:
+        try:
+            url = self.__base_url
+
+            if all:
+                url = f"{self.__base_url}/all"
+            if paused:
+                url = f"{self.__base_url}?paused=true"
+            if online_only:
+                url = f"{self.__base_url}?status=online"
+
+            r = requests.get(url, headers=self.headers)
+            runners_metadata = r.json()
+            return runners_metadata
+
+        except Exception as e:
+            response = requests.Response()
+            response._content = b"Bad Request"
+            response.reason = f"{e}"
+
+    def get_runner_by_id(self, runner_id: str) -> dict:
+        try:
+            url = f"{self.__base_url}/{runner_id}"
+            r = requests.get(url, headers=self.headers)
+            runners_metadata = r.json()
+            return runners_metadata
+
+        except Exception as e:
+            response = requests.Response()
+            response._content = b"Bad Request"
+            response.reason = f"{e}"
